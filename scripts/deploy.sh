d191c61480d7bcaae052535f1a988cc1121d34b5
Teagan Durtschi <teagan@durtschi.com>
2021-06-04 23:16:34 -0500

    separate ci steps from deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,9 @@ set -e
 PROJECT_ROOT="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; cd .. ; pwd -P )"
 
 export_variables_from_file() {
-    if [ ! -f $PROJECT_ROOT/deployment/deploy.env ]; then
+    if [ ! -f $PROJECT_ROOT/scripts/deploy.env ]; then
         echo "😭 ERROR!"
-        echo "🔴 File 'deploy.env' does not exist in directory $PROJECT_ROOT/deployment"
+        echo "🔴 File 'deploy.env' does not exist in directory $PROJECT_ROOT/scripts"
         echo ""
         exit 1
     fi
@@ -20,18 +20,17 @@ run_deploy_from_docker() {
     docker run -it --rm \
         -v $PROJECT_ROOT:$PROJECT_TARGET_DIR \
         -w=$PROJECT_TARGET_DIR \
-        --env-file=$PROJECT_ROOT/deployment/deploy.env \
+        --env-file=$PROJECT_ROOT/scripts/deploy.env \
         frolvlad/alpine-bash \
-        /bin/bash $PROJECT_TARGET_DIR/deployment/deploy-docker.sh
+        /bin/bash $PROJECT_TARGET_DIR/scripts/docker/deploy-docker.sh
 }
 
 main() {
     cd $PROJECT_ROOT
-    export_variables_from_file ./deployment/deploy.env
-    yarn build
-    yarn test
-    yarn e2e:ci
+    
+    ./scripts/ci.sh
 
+    export_variables_from_file ./scripts/deploy.env
     run_deploy_from_docker
 }
 
