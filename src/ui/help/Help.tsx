1d8e3fd29a1c430a4c9e3327e75080c2a166ac19
Teagan Durtschi <teagan@durtschi.com>
2021-08-19 17:15:46 -0500

    Update wording for in-game help

--- src/ui/help/Help.tsx
+++ src/ui/help/Help.tsx
@@ -5,9 +5,11 @@ import ReactMarkdown = require("react-markdown");
 const helpMarkdown = `
 # Rules
 
-The goal of Block Game is to play as many of your pieces (tiles) as possible. A player gets one point for each square of a tile played (For example, the 1x1 tile is worth 1 point, and the 5x1 tile is worth 5 points)
+The goal of Block Game is to play as many of your pieces (tiles) as possible. Bigger tiles are worth more points, and the player with the most points wins.
 
-On a player's turn, a player can either play a tile or pass. You can't play again after passing, so be sure you're out of moves.
+On your turn, you can either play a tile or pass. You can't play any more tiles after passing, so be sure you're out of moves.
+
+The game ends when every player has either passed or played all their tiles.
 
 The rules for playing a tile are:
 
@@ -15,8 +17,6 @@ The rules for playing a tile are:
 * Two tiles can't overlap.
 * Two of the player's tiles CANNOT touch each other ADJACENTLY. 
 * Two of the player's tiles MUST touch each other DIAGONALLY, at least once.
-
-The game ends when every player has either passed or played all their tiles.
 
 # Gameplay
 To play a tile, click on it to pick it up, and place it on the gameboard.
