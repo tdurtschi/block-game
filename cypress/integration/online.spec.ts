0039f396673477f1386e34e21f38dfa07dc1fde0
Teagan Durtschi <teagan@durtschi.com>
2021-10-17 09:55:05 -0400

    failing test for online games

--- cypress/integration/online.spec.ts
+++ cypress/integration/online.spec.ts
@@ -4,9 +4,21 @@ describe("Online game", () => {
         cy.contains("Block Game");
     });
 
-    it("Creates and joins an online game.", () => {
+    it("Creates and views online games.", () => {
         cy.get("[data-online-game]").click();
-        cy.get("[data-new-online-game]").click();
+        cy.get("[data-games-list] li").then((games) => {
+            const numOfExistingGames = games.length;
+
+            cy.get("[data-new-online-game]").click();
+
+            cy.get("[data-games-list] li").should(
+                "have.length",
+                numOfExistingGames + 1
+            );
+        });
+    });
+
+    it("joins an online game.", () => {
         cy.get("[data-join-game]").click();
         cy.get("[data-start-game]").click();
         cy.get("[data-game-board]");
